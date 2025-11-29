3a513a8be6bd4814c7c84ca1f8239785a052c972
Deepak Chandhru N <122144025+Deepakchandhru@users.noreply.github.com>
2025-11-29 19:43:53 +0530

    Modify SocketIO async mode and start server
    Updated SocketIO to use threading for async mode and added missing socketio.run() to start the server.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from pyngrok import ngrok
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode="threading")
 
 users = {}  
 user_languages = {} 
@@ -80,4 +80,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
     public_url = ngrok.connect(port)
     print("Ngrok Tunnel URL:", public_url)
+
     socketio.run(app, host='0.0.0.0', port=port)
